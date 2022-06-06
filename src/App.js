919f5b0408956a89d0f298b9afe6942978c5d1a5
Jessie Zollinger <jzolly@hotmail.com>
2022-06-06 17:18:27 -0400

    css dark mode complete

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="fullContainer">
       <SideBar />
       <div className="dataMain">
-        <div >
+        <div className="top">
           <TopData whichTop="Reviews" data="1,281"/>
           <TopData whichTop="Average Rating" data="4.6"/>
           <TopData whichTop="Sentiment Analysis" data="960"/>
