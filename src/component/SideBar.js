ea4d180d42f6ef6f11feaea42c321ce5ae2c6c29
Jessie Zollinger <jzolly@hotmail.com>
2022-06-07 09:06:02 -0400

    fixed alignment added anchors

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -2,11 +2,11 @@ const SideBar = () => {
     return (
         <div className="sideBar">
         <h2>DashBoard</h2>
-        <h3>Widget</h3>
-        <h3>Review</h3>
-        <h3>Customers</h3>
-        <h3>Online Analysis</h3>
-        <h3>Settings</h3>
+        <a href="https://jquense.github.io/react-widgets/docs/"><h3>Widget</h3></a>
+        <a href="https://www.merriam-webster.com/dictionary/review"><h3>Review</h3></a>
+        <a href="https://www.freecodecamp.org/news/fetch-data-react/"><h3>Customers</h3></a>
+        <a href=""><h3>Online Analysis</h3></a>
+        <a href=""><h3>Settings</h3></a>
         </div>
     );
 }
